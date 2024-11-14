7b50d9b093cf6c8b6dbb62a654a9c9476ba27aa4
Cauan-Silva <anacauansantos@gmail.com>
2024-11-14 18:53:18 -0300

    .

---
+++ Exercicio33_2/src/main/java/Clientes/Cliente.java
@@ -0,0 +1,25 @@
+package Clientes;
+
+public class Cliente {
+    private int codigo;
+    private String nome;
+    private String endereco;
+    private String bairro;
+    private String cpf;
+    private String telefone;
+
+    public Cliente(int codigo, String nome, String endereco, String bairro, String cpf, String telefone) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.endereco = endereco;
+        this.bairro = bairro;
+        this.cpf = cpf;
+        this.telefone = telefone;
+    }
+
+    // Getters e toString()
+    public String toString() {
+        return String.format("Cliente[codigo=%d, nome=%s, endereco=%s, bairro=%s, cpf=%s, telefone=%s]",
+                codigo, nome, endereco, bairro, cpf, telefone);
+    }
+}
